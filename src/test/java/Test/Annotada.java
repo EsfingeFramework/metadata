854eeec8bda952a48e120cc805f88556b5b33e8e
home <home@localhost>
2016-12-05 13:59:02 -0200

    Corrigindo para o Merge

--- src/test/java/Test/Annotada.java
+++ src/test/java/Test/Annotada.java
@@ -1,6 +1,5 @@
 package Test;
 
-<<<<<<< HEAD
 import Test.annotations.AnnotationPropertyEncapsuled;
 import Test.annotations.Entidade;
 import Test.annotations.InTheField;
@@ -14,14 +13,6 @@ public class Annotada {
 	int fieldAnnoted;
 	
 	int fieldNotAnnoted;
-=======
-import net.sf.esfinge.metadata.TestAnnotationReader.Tabela;
-
-@Entidade
-@Tabela(nome="dominio")
-public class Annotada {
-	int field;
->>>>>>> 3851060b60541594d0fd56c768712197f8adce1e
 	
 	@InTheMethod
 	public void methodAnnoted(){}
