7a53bf300305dfe7651aef4600e74a7c0d94377b
Marco Nardes <marconardes@gmail.com>
2017-03-30 09:24:27 -0300

    Update errors

--- src/main/java/net/sf/esfinge/metadata/annotation/container/Processors.java
+++ src/main/java/net/sf/esfinge/metadata/annotation/container/Processors.java
@@ -4,13 +4,13 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Annotation;
 import java.lang.annotation.Retention;
+
+import net.sf.esfinge.metadata.annotation.finder.SearchOnEnclosingElements;
 import net.sf.esfinge.metadata.container.reading.ProcessorsReadingProcessor;
-import net.sf.esfinge.metadata.validate.needsToHave.SearchOnEnclosingElements;
 
 @Retention(RUNTIME)
 @AnnotationReadingConfig(ProcessorsReadingProcessor.class)
 @SearchOnEnclosingElements
-
 public @interface Processors {
 	Class<? extends Annotation> value();
 	ProcessorType type() default ProcessorType.READER_IS_PROCESSOR;
