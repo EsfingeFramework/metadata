f02dd6a123520f8f2a0b08a72d5eb69de4141f56
Eduardo Guerra <eduardoguerra@iMac-de-Suporte.local>
2017-04-11 14:59:23 -0300

    Inicio da classe Repository

--- src/main/java/net/sf/esfinge/metadata/container/MetadataRepository.java
+++ src/main/java/net/sf/esfinge/metadata/container/MetadataRepository.java
@@ -6,7 +6,33 @@ import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
+import net.sf.esfinge.metadata.AnnotationReadingException;
+
 public class MetadataRepository {
+	
+	private Map<Class<?>,Map<AnnotatedElement, Object>> repository = new HashMap<>();
+	
+	private void addContainer(Class<?> containerClass, AnnotatedElement targetElement, Object container) throws AnnotationReadingException{
+		if(!containerClass.isInstance(container)){
+			throw new AnnotationReadingException("The container is not from the container class");
+		}
+		if(!repository.containsKey(containerClass)){
+			repository.put(containerClass, new HashMap<>());
+		}
+		repository.get(containerClass).put(targetElement, container);
+	}
+	
+	public <E> E getContainer(Class<E> containerClass, AnnotatedElement targetElement) throws AnnotationReadingException{
+		if(repository.containsKey(containerClass) && repository.get(containerClass).containsKey(targetElement)){
+			return (E) repository.get(containerClass).get(targetElement);
+		}else{
+			Object newContainer = null; //ler o metadado
+			addContainer(containerClass, targetElement, newContainer);
+			return (E) newContainer;
+		}
+	}
+	
+	
 	private Map<AnnotatedElement,Annotation> repositorio;
 	
 	
