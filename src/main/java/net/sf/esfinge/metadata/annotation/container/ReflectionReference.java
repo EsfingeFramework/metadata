d527a03505c8d609685ad815fbd2114b6f0250ab
Marco Nardes <marconardes@gmail.com>
2017-07-10 11:38:03 -0300

    Update Reflection Reference

--- src/main/java/net/sf/esfinge/metadata/annotation/container/ReflectionReference.java
+++ src/main/java/net/sf/esfinge/metadata/annotation/container/ReflectionReference.java
@@ -4,6 +4,8 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 import net.sf.esfinge.metadata.annotation.validator.ValidFieldType;
 import net.sf.esfinge.metadata.container.reading.ReflectionReferenceReadingProcessor;
@@ -13,7 +15,7 @@ import net.sf.esfinge.metadata.annotation.finder.SearchOnEnclosingElements;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 @AnnotationReadingConfig(ReflectionReferenceReadingProcessor.class)
-@ValidFieldType({Class.class})
+@ValidFieldType({Class.class,Field.class,Method.class})
 @SearchOnEnclosingElements
 
 public @interface ReflectionReference {
