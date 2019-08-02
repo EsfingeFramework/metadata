3ff768a55bf9ff217b2ce13d850a09c3fefc71be
marco <marco@marco-S400CA>
2019-08-02 15:36:54 -0300

    Commit adicionado suporte a parametros no @ReflectionReference

--- src/test/java/net/sf/esfinge/container/processor/parameter/ParameterContainer.java
+++ src/test/java/net/sf/esfinge/container/processor/parameter/ParameterContainer.java
@@ -1,7 +1,10 @@
 package net.sf.esfinge.container.processor.parameter;
 
+import java.lang.reflect.Parameter;
+
 import net.sf.esfinge.metadata.annotation.container.ContainerFor;
 import net.sf.esfinge.metadata.annotation.container.ElementName;
+import net.sf.esfinge.metadata.annotation.container.ReflectionReference;
 import net.sf.esfinge.metadata.container.ContainerTarget;
 
 @ContainerFor(ContainerTarget.PARAMETER)
@@ -9,6 +12,19 @@ public class ParameterContainer {
 	
 	@ElementName
 	private String parameterName;
+
+	@ReflectionReference
+	private Parameter parameter;
+	
+	
+	
+	public Parameter getParameter() {
+		return parameter;
+	}
+
+	public void setParameter(Parameter parameter) {
+		this.parameter = parameter;
+	}
 
 	public String getParameterName() {
 		return parameterName;
